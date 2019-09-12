9124f8949317ccc424f934c01e2f670c18934dc6
Hung Nguyen <hungnguyen@Hungs-Hackintosh.local>
2019-09-12 08:07:32 -0400

    added trie exercises

--- 09-04.py
+++ 09-04.py
@@ -31,3 +31,124 @@ for i in range(n):
 for i in range(q):
     query = input()
     print(findMaxWeight(root, query))
+
+# dna prefix
+
+
+class Node:
+    def __init__(self):
+        self.child = dict()
+        self.common = 0
+
+
+def addWord(root, s):
+    temp = root
+    res = 0
+
+    for level in range(len(s)):
+        char = s[level]
+        if char not in temp.child:
+            temp.child[char] = Node()
+        temp = temp.child[char]
+        temp.common += 1
+        res = max(res, temp.common * (level + 1))
+    return res
+
+
+t = int(input())
+for k in range(t):
+    root = Node()
+    result = 0
+    n = int(input())
+    for i in range(n):
+        result = max(result, addWord(root, input()))
+    print('Case {}: {}'.format(k + 1, result))
+
+# consistentcy checker
+
+
+class Node:
+    def __init__(self):
+        self.child = dict()
+        self.countWord = 0
+
+
+def addWord(root, s):
+    temp = root
+
+    s_prefix_another = True
+    another_prefix_s = False
+
+    for ch in s:
+        if ch not in temp.child:
+            temp.child[ch] = Node()
+            s_prefix_another = False
+
+        temp = temp.child[ch]
+
+        if temp.countWord != 0:
+            another_prefix_s = True
+
+    temp.countWord += 1
+
+    return s_prefix_another or another_prefix_s
+
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    is_inconsistent = False
+    root = Node()
+
+    for _ in range(n):
+        if not is_inconsistent:
+            is_inconsistent = addWord(root, input())
+        else:
+            input()
+
+    print('Case {}: {}'.format(i + 1, 'NO' if is_inconsistent else 'YES'))
+
+# 911 - 9112 => NO
+# 9112 - 911 => NO
+
+# s_is_prefix_another || another_is_prefix_s => True : inconsistent
+# (another: 9112, s: 911)
+
+# "no number is the prefix another number in that data set"
+
+
+# bank password
+class Node:
+    def __init__(self):
+        self.child = dict()
+        self.countPassword = 0
+
+
+def addPassword(root, s):
+    temp = root
+    s_prefix_another = True
+    another_prefix_s = False
+
+    for ch in s:
+        if ch not in temp.child:
+            temp.child[ch] = Node()
+            s_prefix_another = False
+
+        temp = temp.child[ch]
+        if temp.countPassword != 0:
+            another_prefix_s = True
+
+    temp.countPassword += 1
+
+    return s_prefix_another or another_prefix_s
+
+
+n = int(input())
+root = Node()
+is_vulnerable = False
+for i in range(n):
+    if not is_vulnerable:
+        is_vulnerable = addPassword(root, input())
+    else:
+        input()
+print('vulnerable' if is_vulnerable else 'non vulnerable')
